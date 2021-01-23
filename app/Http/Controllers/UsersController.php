87f1ae9ce10efeb3e9d25d8ea5c8ffcbd6f46616
Kevin Burbano <burbanokevin1997@gmail.com>
2021-01-22 20:34:44 -0500

    first commit

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::orderBy('created_at', 'desc')->take(5)->get();
+        return $users;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'name' => 'required',
+            'points' => 'required|numeric',
+            'level' => 'required|numeric'
+        ]);
+        $user = User::create($request->all());
+        return $user;
+    }
+}
